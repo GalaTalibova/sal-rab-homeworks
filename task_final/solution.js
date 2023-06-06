7b33b8e74338e964ede6b0823b37e0d838047fa9
Gala <galya.shemelinina@yandex.ru>
2023-06-06 10:55:11 +0300

    change task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -8,9 +8,7 @@
 // Как результат функции требуется вернуть JSON,
 // cформированный в соответствии с правилами:
 // Объект data содержит все данные
-// В объекте data есть свойства:
-// - client - строка, имя клиента + телефон клиента;
-// - order - объект, содержащий данные о заказе:
+
 //     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
 //     - sum - стоимость заказа с учетом скидок и доставки
 // - goods: массив объектов с информацией о позициях заказа:
@@ -34,20 +32,31 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {// В объекте data есть свойства: 
+        client: '', // - client - строка, имя клиента + телефон клиента;
+        order: {},// - order - объект, содержащий данные о заказе:
+        goods: [] 
+        }
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat;
+   
+    console.log(data.order.address);
 
-    data.client = 'Иван';
+    data.order.sum = sum;
+    console.log(data.order.sum);
 
-    let jsonData = JSON.stringify(data);
+    data.client = name + ' ' + phone;
 
+    let jsonData = JSON.stringify({data});
+    
     return jsonData;
-}
+    }
+
+    
+    
